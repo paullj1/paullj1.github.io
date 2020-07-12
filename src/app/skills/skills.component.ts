b28ae40ae8a030f4a64ba47184ea60baaf0b7c07
Paul Jordan <paullj1@gmail.com>
2020-07-12 10:18:30 -0400

    Add youracclaim badges to certifications

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -75,11 +75,26 @@ export class SkillsComponent implements OnInit {
       icon: "edit",
       color: "purple",
       bullets: [
-        "Offensive Security Certified Professional (OSCP); 2020, Certificate ID: OS-101-21294",
-        "GIAC Certified Exploit Researcher and Advanced Penetration Tester (GXPN); 2020 - Present, Analyst Number: 2402",
-        "Certified Information System Security Professional (CISSP); 2014 - Present, Member Number: 482166",
-        "GIAC Certified Incident Handler (GCIH); 2013 - 2017",
-        "CompTIA Security+; 2012 - 2015",
+        { 
+          "text": "Offensive Security Certified Professional (OSCP); 2020, Certificate ID: OS-101-21294",
+          "url": "https://www.youracclaim.com/badges/683557d2-42ea-4b77-a188-b5cba4afc954/public_url"
+        },
+        {
+          "text": "GIAC Certified Exploit Researcher and Advanced Penetration Tester (GXPN); 2020 - Present, Analyst Number: 2402",
+          "url": "https://www.youracclaim.com/badges/e46a495c-c824-4453-b31d-19284efae4ce/public_url"
+        },
+        {
+          "text": "Certified Information System Security Professional (CISSP); 2014 - Present, Member Number: 482166",
+          "url": "https://www.youracclaim.com/badges/9834e3eb-e7e5-4b6c-91ad-79c0e0c75a79/public_url"
+        },
+        {
+          "text": "GIAC Certified Incident Handler (GCIH); 2013 - 2017",
+          "url": "https://www.youracclaim.com/badges/ad2b7ebe-d8ce-4c80-8ef8-f00518aa7f38/public_url"
+        },
+        {
+          "text": "CompTIA Security+; 2012 - 2015",
+          "url": "https://www.youracclaim.com/badges/6ebe7994-0b1c-4073-bd69-6c8fb45bd17f/public_url"
+        }
       ]
     },
   ];
